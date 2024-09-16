f8399f90807a9e82a52485774d0f04dc97c658df
Saveli Matjuhhin <saveli.matjuhhin@tuni.fi>
2024-09-16 13:58:21 +0300

    exercises 1.3-1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -15,14 +15,15 @@ const Part = ({ part, exercises }) => {
 const Content = ({ parts }) => {
     return (
         <div>
-            {parts.map((part, index) => (
+            {parts.map(((part, index) =>
                 <Part key={index} part={part.name} exercises={part.exercises} />
             ))}
         </div>
     )
 }
 
-const Total = ({ total }) => {
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
     return (
         <p>
             Total number of exercises: {total}.
@@ -31,19 +32,29 @@ const Total = ({ total }) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-
-    const parts = [
-        {name: 'Fundamentals of React', exercises: 10},
-        {name: 'Using props to pass data', exercises: 7},
-        {name: 'State of a component', exercises: 14}
+    const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
     ]
+  }
 
     return (
         <div>
-            <Header course={course}/>
-            <Content parts={parts}/>
-            <Total total={parts.reduce((sum, part) => sum + part.exercises, 0)}/>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
         </div>
     )
 }
