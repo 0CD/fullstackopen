6bce8753e0680b9c88d8892be1c699ff36095549
Saveli Matjuhhin <saveli.matjuhhin@tuni.fi>
2024-12-01 14:56:42 +0200

    Exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
+const loginRouter = require("./controllers/login");
 
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
@@ -20,6 +21,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogRouter)
 
 app.use(middleware.errorHandler)
