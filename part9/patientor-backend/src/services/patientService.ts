1f4e1ed1cf22e837cc3405444503b01651b4cf77
Saveli Matjuhhin <saveli.matjuhhin@tuni.fi>
2025-01-02 20:38:31 +0200

    Exercises 9.8-9.13

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,33 @@
+import patients from "../../data/patients";
+import { Patient, PublicPatient, NewPatient } from "../types";
+import { v1 as uuid } from "uuid";
+
+const getEntries = (): Array<Patient> => {
+  return patients;
+};
+
+const getPublicEntries = (): Array<PublicPatient> => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addPatient = (entry: NewPatient): Patient => {
+  const newPatientEntry = {
+    id: uuid(),
+    ...entry,
+  };
+
+  patients.push(newPatientEntry);
+  return newPatientEntry;
+};
+
+export default {
+  getEntries,
+  getPublicEntries,
+  addPatient,
+};
